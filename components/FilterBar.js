e3b0180a35a5fd1f290cf561f0b13900e4fba899
AFortune <aaron.fortune@icloud.com>
2018-01-16 16:16:17 -0500

    Have fade in and out working.

--- components/FilterBar.js
+++ components/FilterBar.js
@@ -1,5 +1,61 @@
 import React, {Component} from 'react'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import './FilterBar.css'
+
+const showHider = (toggleValue, Component) => toggleValue ? Component : null;
+
+const FadeInTimeout = 800;
+const FadeOutTimeout = 800;
+
+
+const ClosedSection = props =>
+      <div className="filter-bar-transition-wrapper">
+      <CSSTransitionGroup
+                transitionName="filter-bar-fade"
+	    	 transitionEnterTimeout={FadeInTimeout}
+	    	 transitionLeaveTimeout={FadeOutTimeout}>
+    {!props.open &&
+     <div key="closed-section" className="wrapper-class">
+     <div  className={`filter-bar-closed-section `}>
+	<div className="filter-bar-filter-area">
+	  <div className="filter-bar-filter-list">
+	    <div className="filter-button-container">
+	      <div onClick={props.toggle} className="filter-circle-button" >▼</div>
+	      <div className="filter-button-text">FILTERS</div>
+	    </div>
+	  </div>
+	</div>
+	<div className="filter-bar-notify">
+	  <div className="filter-bar-notify-button">⋈</div>
+	  <div className="filter-bar-notify-text">NOTICE</div>
+	</div>
+	</div>
+     </div>}
+    </CSSTransitionGroup>
+    </div>;
+
+const OpenSection = props =>
+      <div className="filter-bar-transition-wrapper">
+      <CSSTransitionGroup
+                transitionName="filter-bar-fade"
+	    	 transitionEnterTimeout={FadeInTimeout}
+	    	 transitionLeaveTimeout={FadeOutTimeout}>
+    {props.open &&
+     <div key="open-section" className="wrapper-class">
+     <div  className={`filter-bar-open-section  ${props.open ? " ": ""}`}>
+	<div className="filter-bar-selection-title" >
+	  <div className="filter-bar-selection-title-large">CHOOSE</div>
+	  <div className="filter-bar-selection-title-small"> Your Filters</div>
+	</div>
+	<div className="filter-bar-mask-area">
+	  <div className="filter-bar-mask-button">
+	  <span className="filter-bar-mask-button-icon">⋈</span> HOW TO CHOOSE A MASK</div>
+	  <div onClick={props.toggle} className="filter-bar-close-button filter-circle-button">❌</div>
+	  </div>
+	  </div>
+       </div>}
+    </CSSTransitionGroup>
+    </div> ;
 
 class FilterBar extends Component {
     constructor(props) {
@@ -22,31 +78,8 @@ class FilterBar extends Component {
       <div className="filter-bar-logo">Amanuensis</div>
     </div>
     <div className="filter-bar-container">
-      <div className={`filter-bar-closed-section ${this.state.open ? "transparent invisible": ""}`}>
-	<div className="filter-bar-filter-area">
-	  <div className="filter-bar-filter-list">
-	    <div className="filter-button-container">
-	      <div onClick={this.toggle} className="filter-circle-button" >▼</div>
-	      <div className="filter-button-text">FILTERS</div>
-	    </div>
-	  </div>
-	</div>
-	<div className="filter-bar-notify">
-	  <div className="filter-bar-notify-button">⋈</div>
-	  <div className="filter-bar-notify-text">NOTICE</div>
-	</div>
-      </div>
-      <div className={`filter-bar-open-section ${this.state.open ? "opaque visible-flex": ""}`}>
-	<div className="filter-bar-selection-title" onClick={this.toggle} >
-	  <div className="filter-bar-selection-title-large">CHOOSE</div>
-	  <div className="filter-bar-selection-title-small"> Your Filters</div>
-	</div>
-	<div className="filter-bar-mask-area">
-	  <div className="filter-bar-mask-button">
-	  <span className="filter-bar-mask-button-icon">⋈</span> HOW TO CHOOSE A MASK</div>
-	  <div onClick={this.toggle} className="filter-bar-close-button filter-circle-button">❌</div>
-	  </div>
-      </div>
+		<ClosedSection toggle={this.toggle} open={this.state.open} />
+		<OpenSection toggle={this.toggle} open={this.state.open} />
     </div>
   </div>
   <div className={`filter-bar-selection-area ${this.state.open ? "open": ""}`}>
